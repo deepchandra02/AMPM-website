366b923ec1028ca6a93eece2d77c10bf2d611c3f
Sameeksha Shah <sameekshashah@Sameekshas-MacBook-Air.local>
2022-05-27 18:03:41 +0530

    started hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mpurple: '#25215c',
+      }
+    },
   },
   plugins: [],
 }
