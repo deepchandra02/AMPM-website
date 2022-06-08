2200aada2ffac4d2e27f7a667cc1bf2547334c3f
“deepchandra02” <thedeepchandra@gmail.com>
2022-06-08 14:40:14 +0300

    fixed hero image top being hidden by navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import image from '../images/1.1.jpg';
+import React from "react";
+import { Link } from "react-router-dom";
+import image from "../images/1.1.jpg";
 
 const Hero = () => {
   return (
-    <div className='bg-slate-500 h-screen flex flex-col justify-center items-center'>
-      <img src={image} alt="hero" className='h-full top-6.5' />
-      <div className='m-auto absolute '>
+    <div className="bg-slate-500 pt-24 h-screen w-full flex flex-col justify-center items-center">
+      <img src={image} alt="hero" className="h-full top-6.5" />
+      <div className="m-auto absolute ">
         <h1 className='lg:text-5xl md:text-3xl sm:text-1xl text-1xl font-["Montserrat"] text-mpurple mb-14 text-align:center'>
-        Exceeding Expectations <br></br>One Property at A Time
+          Exceeding Expectations <br></br>One Property at A Time
         </h1>
-        <div className='flex justify-between'>
-          <Link to="/" className='py-6 px-10 font-["Montserrat"] text-mpurple bg-none border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'>
+        <div className="flex justify-between">
+          <Link
+            to="/"
+            className='py-6 px-10 font-["Montserrat"] text-mpurple bg-none border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'
+          >
             Contact Now
           </Link>
-          <Link to="/" className='py-6 px-10 font-["Montserrat"] text-mpurple border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'>
+          <Link
+            to="/"
+            className='py-6 px-10 font-["Montserrat"] text-mpurple border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'
+          >
             Know More
           </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <Hero />
       <AboutSection />
-      <MV/>
+      <MV />
     </>
   );
 };
