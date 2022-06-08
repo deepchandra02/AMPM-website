dccd951f74bab99c75df5b12458b5d77a037ffcf
Sameeksha Shah <sameekshashah@Sameekshas-MacBook-Air.local>
2022-06-08 10:27:29 +0530

    3sections done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center h-16 bg-black text-white">
+    <div className="flex justify-center items-center h-16 bg-slate-500 text-white">
       <p>Copyright © 2022 Al Madar - All rights reserved.</p>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ const Hero = () => {
     <div className='bg-slate-500 h-screen flex flex-col justify-center items-center'>
       <img src={image} alt="hero" className='h-full top-6.5' />
       <div className='m-auto absolute '>
-        <h1 className='lg:text-5xl md:text-3xl sm:text-1xl text-1xl font-montserrat text-mpurple mb-14 text-align: center'>
+        <h1 className='lg:text-5xl md:text-3xl sm:text-1xl text-1xl font-["Montserrat"] text-mpurple mb-14 text-align:center'>
         Exceeding Expectations <br></br>One Property at A Time
         </h1>
         <div className='flex justify-between'>
-          <Link to="/" className='py-6 px-10 font-montserrat text-mpurple bg-none border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'>
+          <Link to="/" className='py-6 px-10 font-["Montserrat"] text-mpurple bg-none border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'>
             Contact Now
           </Link>
-          <Link to="/" className='py-6 px-10 font-montserrat text-mpurple border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'>
+          <Link to="/" className='py-6 px-10 font-["Montserrat"] text-mpurple border-2 border-mpurple rounded-full text-3xl hover:bg-mpurple hover:text-white transition duration-300 ease-in out flex item-center'>
             Know More
           </Link>
         </div>
